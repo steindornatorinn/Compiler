7a625c97938665db2d95d966f326339f01fc0971
GKRfan2003 <fuckyouj@gmail.com>
2016-02-10 11:16:52 -0500

    Fixed execute script

--- src/execute.sh
+++ src/execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-Program = $1
-python compiler < Program > program.code
-python interpreter < program.code
+Program=$1
+python compiler.py < $Program > program.code
+python interpreter.py < program.code
