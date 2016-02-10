17e1d4ef23733750d73f71b13b145ba2cb740d0e
GKRfan2003 <fuckyouj@gmail.com>
2016-02-10 10:19:03 -0500

    Created the parser class

---
+++ src/parser.py
@@ -0,0 +1,90 @@
+from token import *
+import sys
+class Parser(object):
+    '''A parser for our programming language'''
+    #Current token
+    token = None
+    def __init__(self, lexer):
+        '''Initialization function, takes a lexical analyzer as a argument'''
+        self.lexer = lexer
+
+    def parse(self):
+        '''A parser parses'''
+
+    def statements(self):
+        '''Parse statements'''
+        while True:
+            #If we recieve a end token, stop
+            if self.accept(Token_codes.END):
+                sys.exit(0)
+            #Else parse a single statment
+            else:
+                self.statement()
+
+    def statement(self):
+        '''Parse a single statement'''
+        #statement -> Print ID | expression
+        if self.accept(Token_codes.PRINT):
+            self.expect(Token_codes.ID)
+            print "PUSH " + self.token.lexeme
+            print "PRINT"
+        else:
+            self.expression()
+        #all statements must be ended with a semicolon
+        self.expect(Token_codes.SEMICOL)
+
+    def expression(self):
+        '''Parse a single expression'''
+        #expression -> term | term + expression | term - expression
+        self.term()
+        if(self.accept(Token_codes.ADD)):
+            self.expression()
+            print "ADD"
+        if(self.accept(Token_codes.SUB)):
+            self.expression()
+            print "SUB"
+
+    def term(self):
+        '''Parse a single term'''
+        #term -> factor| factor * term
+        self.factor()
+        if(self.accept(Token_codes.MULT)):
+            self.term()
+            print "MULT"
+
+    def factor(self):
+        '''Parse a single factor'''
+        # factor -> ID | INT | (EXPRESSION)
+        if(self.accept(Token_codes.ID)):
+            print "PUSH " + self.token.lexeme
+        elif(self.accept(Token_codes.INT)):
+            print "PUSH " + self.token.lexeme
+        elif(self.accept(Token_codes.LPAREN)):
+            self.expression()
+            self.expect(Token_codes.RPAREN)
+
+    def error(self, message):
+        '''Print a error message and exit'''
+        print message
+        sys.exit(0)
+
+    def accept(self, token):
+        '''If the current token is the same as the passed one, return true and get the next one'''
+        if self.token == token:
+            self.token = self.lexer.nextToken()
+            if self.token == Token_codes.ERROR: 
+                error("Lexical Error")
+            return True
+        else:
+            return False
+
+    def expect(self, token):
+        '''Ensure the current token is the same one that we expect it to be'''
+        accepted = self.accept(token)
+        if accepted:
+            return True
+        else:
+            error("Unexpected token" + self.token)
+            return False
+    
+
