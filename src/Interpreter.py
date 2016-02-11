45e515d0ff255a958c540f8fb97e1e92f85b292a
Omarv <omarkval@gmail.com>
2016-02-11 00:14:35 +0000

    laga indents fkn notepad

--- src/Interpreter.py
+++ src/Interpreter.py
@@ -1,5 +1,5 @@
 import sys
-#Stack class taken from http://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementingaStackinPython.html
+# Stack class taken from http://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementingaStackinPython.html
 class Stack:
      def __init__(self):
          self.items = []
@@ -24,40 +24,40 @@ class Interpreter:
     s = Stack()
     assign = {}
     checkpush = False
-    # push function check if op exists in dictionary used for variable 
-	def Push(self, op):
+    # push function check if op exists in dictionary used for variable
+    def Push(self, op):
         if op in self.assign:
             op = self.assign[op]
         self.s.push(op)
    
-	def Add(self):
+    def Add(self):
         number = self.s.pop()
         number2 = self.s.pop()
         sums = number + number2
         self.s.push(sums)
     
-	def Sub(self):
+    def Sub(self):
         number = self.s.pop()
         number2 = self.s.pop()
         sums = number2 - number
         self.s.push(sums)
     
-	def Mult(self):
+    def Mult(self):
         number = self.s.pop()
         number2 = self.s.pop()
         sums = number * number2
         self.s.push(sums)
     
-	def Assign(self):
+    def Assign(self):
         number = self.s.pop()
         var = self.s.pop()
         self.assign[var] = number    
         self.s.push(number)
     
-	def Print(self):
+    def Print(self):
         print (self.s.peek())
     # stdin function that spilt on every word and check every word for keyword
-	def read_in(self):
+    def read_in(self):
         for lines in sys.stdin:
             lines = lines.strip()
             words = lines.split()
@@ -72,8 +72,8 @@ class Interpreter:
                     self.checkpush = True
                 else:
                     self.checkWordForOp(word)
-    #check word for op keyword
-	def checkWordForOp(self,word):
+    # check word for op keyword
+    def checkWordForOp(self,word):
         if(word == "ADD"):
             if (self.checkStackForTwo()):
                 self.Add()
@@ -107,8 +107,8 @@ class Interpreter:
         else:
             print("Error for operator: "+word);
             sys.exit(0)
-    #check if stack has 2 or more items
-	def checkStackForTwo(self):
+    # check if stack has 2 or more items
+    def checkStackForTwo(self):
         if(self.s.size() > 1):
             return True
         else:
